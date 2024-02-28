518a4e8858b738622b403222e70622bc356d770d
alekhyak <alekhyak@snvsoftwaresolutions.onmicrosoft.com>
2024-02-28 15:54:59 +0530

    confirmlocation

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FolderPage } from './folder.page';
+import { ConfirmpickupComponent } from './confirmpickup/confirmpickup.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FolderPage
+    component: ConfirmpickupComponent
   }
 ];
 
