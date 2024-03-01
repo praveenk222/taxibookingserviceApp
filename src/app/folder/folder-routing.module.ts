7c2444d1297515899804fb94bfa1a8a13ea3dd0b
EgurapuPravalika <pravalikae@snvsoftwaresolutions.onmicrosoft.com>
2024-03-01 19:32:49 +0530

    my taxi book

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path:'findride',
     component:FindyourrideComponent
   }
+  ,{
+    path:'continueride',
+    component:ContinuerideComponent
+  }
 ];
 
 @NgModule({
