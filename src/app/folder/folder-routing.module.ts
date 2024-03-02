adc55c79e7d500923a955a4c67a3d65791e12461
praveenk222 <praveenkadma222@gmail.com>
2024-03-02 19:05:34 +0530

    lll

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -6,19 +6,26 @@ import { ChooserideComponent } from './chooseride/chooseride.component';
 import { HomeComponent } from './home/home.component';
 import { FindyourrideComponent } from '../findyourride/findyourride.component';
 import { ContinuerideComponent } from '../continueride/continueride.component';
+import { WheretoComponent } from './whereto/whereto.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },{
+  },
+  {
   
     path:'continueride',
     component:ContinuerideComponent
+  },
+  {  
+    path:'location',
+    component:WheretoComponent
   }
 ];
 
 @NgModule({
-  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class FolderPageRoutingModule {}
