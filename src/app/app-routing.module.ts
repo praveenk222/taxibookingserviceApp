a77d81bf61948a7e73d91c9be67cffae12675961
praveenk222 <praveenkadma222@gmail.com>
2024-03-03 00:17:24 +0530

    dd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {  
     path: '',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'mybooking',
+    loadChildren: () => import('./page/mybooking/mybooking.module').then( m => m.MybookingPageModule)
   },
+
   
 
   
