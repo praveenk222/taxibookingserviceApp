61a0add7f491334f8c92e39ce86aba189cf143e6
hajimalanbaba <Hajibaba@snvsoftwaresolutions.onmicrosoft.com>
2024-03-11 18:41:07 +0530

    user

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -1,11 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FolderPageRoutingModule } from './folder-routing.module';
-
 import { FolderPage } from './folder.page';
 import { ChooserideComponent } from './chooseride/chooseride.component';
 import { HomeComponent } from './home/home.component';
--- src/app/folder/user/user.component.ts
+++ src/app/folder/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
+
 
 @Component({
   selector: 'app-user',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent  implements OnInit {
   
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
